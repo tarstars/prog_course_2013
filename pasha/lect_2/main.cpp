9a9c1c06207361d7145af8f9ffca37a568104ef7
Pasha <nikitin.pavel.a@gmail.com>
2013-02-19 15:08:25 +0400

    strem

--- pasha/lect_2/main.cpp
+++ pasha/lect_2/main.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-  int a=5;
-  int b=10;
-  int *pa;
-  pa=&a;
-  int &x=a; //new additional name of variable a
-  cout<<"a="<<a<<endl;
-  cout<<"x="<<x<<endl;
-  cout<<"b="<<b<<endl;
-  cout<<"&a="<<&a<<endl; //nomber(adress) of cell in memory of first element of a
-  cout<<"pa="<<pa<<endl;
-  cout<<"value of variable, which adress lies in pa is"<<*pa<<endl;
+  double val;
+  while(cin>>val){
+    cout<<val*2<<" ";
+  }
+  return 0;
 }
