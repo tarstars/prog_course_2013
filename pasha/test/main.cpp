5becc743a16e0eea3ccac08c419c0848dca5f663
unknown <Pasha@.(none)>
2013-03-03 12:17:24 +0400

    test

---
+++ pasha/test/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int i;
+  for(i=6; i<10; i=i+1){
+    if(i==6){
+      cout<<i<<" ";
+    }
+    elseif(i==7){
+      cout<<i+10<<" ";
+    }
+    else{
+      cout<<i<<" ";
+    }
+  }
+  return 0;
+}
