66e113e908608d0e8766ee02e3de70e2ec4dea3b
unknown <Pasha@.(none)>
2013-03-06 08:55:17 +0400

    from tars lectures

---
+++ pasha/lect_4/calculator/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  if (argc < 4) {
+    cerr << "usage: plus arg1 arg2 arg3" << endl;
+    return 1;
+  }
+  
+  ifstream larg(argv[1]), rarg(argv[2]);
+  double x, y;
+  ofstream dest(argv[3]);
+
+  while((larg >> x) && (rarg >> y)) {
+    dest << (x + y) << endl;
+  }
+
+  return 0;
+}
+
+  // ofstream dest(argv[1]);
+  // for(int i = 1; i < 2000; i *= 2) {
+  //   dest << i << endl;
+  // }
+
