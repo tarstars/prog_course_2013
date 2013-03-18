3ba4ca12fdf9038cefcf79833f56ef85919b1fb5
unknown <Pasha@.(none)>
2013-03-18 18:55:45 +0400

    update

--- pasha/lect_2/next_adv/main.cpp
+++ pasha/lect_2/next_adv/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 bool next(int *pb, int *pe){
-  int j=pe-pb, n1=0;
+  int j=pe-pb, n1=0, j1;
   
   for(int i=0; i<=j; ++i) {
     cout<<*(pb+i); //вывод исходного массива
@@ -11,7 +11,12 @@ bool next(int *pb, int *pe){
   
   while((((pb[j-1]==0) && (pb[j]==1))!=1)  &&  (j>=1)) {
     --j;
-    if(pb[j]==1) {++n1;} //подсчёт кол-ва 1
+    if(pb[j]==1) {
+      ++n1; //подсчёт кол-ва 1
+      if(n1==1) {
+	j1=j; // место первой 1
+      }
+    }
   }
   if(j==0) {
     cout<<"???\n"<<"Операция next невозможна!"<<endl;
@@ -19,14 +24,14 @@ bool next(int *pb, int *pe){
   }
   else {
     pb[j-1]=1; pb[j]=0;
-    
+
     int nost=0;
     for(int k=j-2; k>=0; --k) {
       if(pb[k]==1){++nost;} //подсчёт кол-ва 1 после первого 0
     }
-    if(nost==0) {
-      for(int i=j+1; i<=j+n1-1; ++i) {
-	pb[i]=0; *(pe-(i-1-j))=1; //сдвиг оставшихся 1 в самый младший разряд
+    if((nost==0) & (n1>=1) & (n1<pe-pb-j-1)) {
+      for(int i=j1; i>=j+1; --i) {
+	pb[i]=0; *(pe-(j1-i))=1; //сдвиг оставшихся 1 в самый младший разряд
       }
     }
 
@@ -40,7 +45,7 @@ bool next(int *pb, int *pe){
 
 int main() {
   
-  int a[]={0,0,1,1,1,0,};
+  int a[]={0,0,0,1,1,1,1,1,1};
   int l=sizeof(a)/sizeof(int);
   int *pb=a;
   int *pe=a+l-1;
