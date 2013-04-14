c92c2d10a80aa88ba57cd1c708a1d79de56d4d85
unknown <Pasha@.(none)>
2013-04-14 18:30:17 +0400

    update

--- pasha/lect_8/convol/main.cpp
+++ pasha/lect_8/convol/main.cpp
@@ -5,9 +5,6 @@
 using namespace std;
 
 int main() {
-
-	//fftw_complex *psource=(fftw_complex *)fftw_malloc(h*w*sizeof(fftw_complex));
-	//fftw_free(psource);
 	
 	int H=100; W=100;
 	
@@ -37,27 +34,57 @@ int main() {
 		}
 	}
 	b.save("b.png");
+	//____________________________________________________
 	
 	QImage img("a.png");
 	int wi = img.width();
 	int hi = img.height();
 	
-	Matrix a(hi,wi), aF(hi,wi), bF(hi,wi), c(hi,wi);
-	fftw_plan pln = fftw_plan_dft_2d(&a, &aF, FFTW_FORWARD, FFTW_ESTIMATE);
+	Matrix a(hi,wi), a_F(hi,wi), b_F(hi,wi), c(hi,wi);
+	
+	fftw_complex *pa = (fftw_complex *)fftw_malloc(hi*wi*sizeof(fftw_complex));
+	fftw_complex *pa_F = (fftw_complex *)fftw_malloc(hi*wi*sizeof(fftw_complex));
+	
+	fftw_plan pln = fftw_plan_dft_2d(hi, wi, pa, pa_F, FFTW_FORWARD, FFTW_ESTIMATE);
+	//____________________________________________________
 	
 	a.load_matrix("b.png");
+	for(int p=0 ; p<hi; p++) {
+		for(int q=0; q<wi; q++) {
+			(complex<double>&) pa[p*w+q] = a.at(p,q);
+		}
+	}
 	fftw_execute(pln);
-	save_matrix(aF, "bF.png");
-	bF=aF; //т.к. aF будет будет перезаписано планом
+	for(int p=0 ; p<hi; p++) {
+		for(int q=0; q<wi; q++) {
+			a_F.at(p,q) = (complex<double>&) pa_F[p*w+q];
+		}
+	}
+	save_matrix(a_F, "bF.png");
+	b_F = a_F; //т.к. aF будет будет перезаписано планом
+	//____________________________________________________
 	
 	a.load_matrix("a.png");
+	for(int p=0 ; p<hi; p++) {
+		for(int q=0; q<wi; q++) {
+			(complex<double>&) pa[p*w+q] = a.at(p,q);
+		}
+	}
 	fftw_execute(pln);
-	save_matrix(aF, "aF.png");
+	for(int p=0 ; p<hi; p++) {
+		for(int q=0; q<wi; q++) {
+			a_F.at(p,q) = (complex<double>&) pa_F[p*w+q];
+		}
+	}
+	save_matrix(a_F, "aF.png");
+	//____________________________________________________
 	
-	c.mult(aF,bF);
+	c.mult(a_F, b_F);
 	save_matrix(c, "c.png");
 	
 	fftw_destroy_plan(pln);
+	fftw_free(pa);
+	fftw_free(pa_F);
 	
 	return 1;
 }
