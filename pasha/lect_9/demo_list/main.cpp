b9696a48de323ba499ecabb235a83e505b5c2805
Pasha <nikitin.pavel.a@gmail.com>
2013-04-16 15:10:09 +0400

    lect_09

---
+++ pasha/lect_9/demo_list/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <list>
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+ostream& print(ostream& os, T itb, T ite) {
+	for(; itb != ite; ++itb) {
+		os<<*itb<<" ";
+	}
+	return os;
+}
+
+int main() {
+	int n, t1=0, t2=0;
+	
+	cout<<"Введите кол-во букв: ";
+	cin>>n;
+	vector<list<char> > dat(3);
+	
+	for(int i=0; i<n; ++i) {
+		dat[0].push_back('a'+i);
+	}
+	for(int i=0; i<3; ++i) {
+		print(cout, dat[i].begin(), dat[i].end()) << endl;
+		cout<<"_________________"<<endl;
+	}
+	
+	while((t1==0 || t1==1 || t1==2) && (t2==0 || t2==1 || t2==2)) {
+		cout<<"Перестановка с t1 на t2: ";
+		cin>>t1>>t2;
+		dat[t2].push_back(dat[t1].back());
+		dat[t1].pop_back();
+		for(int i=0; i<3; ++i) {
+			print(cout, dat[i].begin(), dat[i].end()) << endl;
+			cout<<"_________________"<<endl;
+		}
+	}
+}
