bbc8ab69347cdd440013ac837f599c66e4cf2bc2
Arseniy Trushin <142857@mail.ru>
2013-10-08 15:06:50 +0400

    main: vector demo

--- crystal/main.cpp
+++ crystal/main.cpp
@@ -1,11 +1,29 @@
+#include "vec3.h"
+#include "Tensor4.h"
+#include "matrix.h"
+#include "util.h"
+
+#include <vector>
+
+using namespace std;
+
+class SolPart {
+  double v;
+  Vec3 q;
+public:
+  SolPart(){}
+  SolPart(double x, double y, double z) : q(x, y, z) {}
+};
+
 int main() {
   Vec3 n=Vec3(1, 1, 0).normalized();
-  Tensor4 tens = makeTetragonalMaterialTensor(5.6e10, 5.145e10, 2.2e10, 10.6e10, 2.65e10, 6.6e10);
-  Mat3 chrMat = christoffel(tens, n);
-  vector<SolPart> sols = solveChristoffel(chrMat);
+  Tensor4 tens = makeTetragonalTensor(5.6e10, 5.145e10, 2.2e10, 10.6e10, 2.65e10, 6.6e10);
+  Matrix chrMat = christoffel(tens, n);
+  vector<SolPart> sols(3); // = solveChristoffel(chrMat);
+  SolPart a = sols[1];
 }
 
-//Андрей - MatrixToPoly
-//Настя - нормализация вектора
-//Таня - eval
+//Андрей - solveChristoffel
+//Настя - тесткейсы для новых функций
+//Таня - eval, operator*, det
 //CalcPol - Паша
