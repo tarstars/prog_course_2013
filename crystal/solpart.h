1e3c44845ff3c3ceb871d354f18175c51e69d092
Koptsow Dmitriy <suslic2012@gmail.com>
2013-10-22 12:17:53 +0400

    crystal: povray template, solpart fix

--- crystal/solpart.h
+++ crystal/solpart.h
@@ -7,9 +7,17 @@ public:
   SolPart(){}
   //  SolPart(double x, double y, double z) : q(x, y, z) {}
  SolPart(double vv, Vec3 qq): v(vv), q(qq) {}
-  void Put(double vv, Vec3 qq){ 
+  void Put(double vv, Vec3 qq){
     v = vv;
     q = qq;
+  }
+  Vec3 getVec() const
+  {
+      return q;
+  }
+  double getV() const
+  {
+      return v;
   }
 
   friend std::ostream& operator<<(std::ostream&, const SolPart& r);
