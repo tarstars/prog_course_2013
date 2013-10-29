7e51f49e4e30ad9ff9da8d48a3b60496a0755051
Koptsow Dmitriy <suslic2012@gmail.com>
2013-10-29 18:24:05 +0400

    povray API to util.h

--- crystal/main.cpp
+++ crystal/main.cpp
@@ -3,14 +3,104 @@
 #include "matrix.h"
 #include "solpart.h"
 #include "util.h"
+#include "povray_templates.h"
 
 #include <cmath>
 #include <vector>
 
 using namespace std;
 
+///////////////////////povray test plane cut////////////////////
+void test_cut(ostream& os, const Vec3& vec)
+{
+  Vec3 n = vec.normalized();
+    Tensor4 tens = makeTetragonalTensor(5.6e10, 5.145e10, 2.2e10, 10.6e10, 2.65e10, 6.6e10);
+    double rho = 5.96e3;
+    Matrix chrMat = christoffel(tens, n);
+    vector<SolPart> sols = solveChristoffel(chrMat,rho);
 
+    for(size_t l=0;l<sols.size();++l)
+    {
+        Vec3 speed(2000*n.at(0)/sols.at(l).getV(),
+                   2000*n.at(1)/sols.at(l).getV(),
+                   2000*n.at(2)/sols.at(l).getV());
 
+        outputPovraySphere(os,speed);
+    }
+}
+
+void test_cuts(const char* filebase)
+{
+    char fname[256];
+    memset(fname,0,sizeof(fname));
+    strcat(fname,filebase);
+    strcat(fname,".pov");
+    ofstream povfile;
+    povfile.open(fname,ofstream::out);
+    povfile<<povray_templates::header;
+    povfile<<povray_templates::coords;
+
+    /*test_cut(povfile,Vec3(0,0,1));
+    test_cut(povfile,Vec3(0,0,-1));
+    test_cut(povfile,Vec3(0,1,0));
+    test_cut(povfile,Vec3(0,-1,0));
+    test_cut(povfile,Vec3(1,1,0));
+    test_cut(povfile,Vec3(-1,-1,0));*/
+
+    for(int i=0;i<720;++i)
+    {
+        test_cut(povfile,Vec3(cos(2*M_PI*i/360),sin(2*M_PI*i/360),0));
+        test_cut(povfile,Vec3(0,cos(2*M_PI*i/360),sin(2*M_PI*i/360)));
+    }
+
+    povfile.close();
+
+    povray_templates::make_base(filebase,100,1);
+}
+///////////////////////povray test plane cut////////////////////
+
+///////////////////////povray test rotation/////////////////////
+void test_rotation(const char* filebase)
+{
+    char fname[256];
+    memset(fname,0,sizeof(fname));
+    strcat(fname,filebase);
+    strcat(fname,".pov");
+    ofstream povfile;
+    povfile.open(fname,ofstream::out);
+    povfile<<povray_templates::header;
+    povfile<<povray_templates::coords;
+
+    int n = 100;
+    for(int p = 0; p < n; ++p) {
+      double z = -1 + 2. * p / n;
+      for(int q = 0; q < n; ++q) {
+    double phi = 2 * M_PI * q / n;
+    double r = sqrt(1 - z*z);
+    Vec3 v(r*cos(phi), r*sin(phi),z);
+    try {
+      test_cut(povfile, v);
+    } catch(string msg) {
+      cerr << "problem: " << msg << " p = " << p << " q = " << q << endl;
+    }
+      }
+    }
+
+    povfile.close();
+}
+///////////////////////povray test rotation/////////////////////
+
+int main()
+{
+  try {
+    test_cuts("povray");
+  } catch(string msg) {
+    cerr << "error: " << msg << endl;
+  }
+    return 0;
+}
+
+/*
 int main() {
   Tensor4 tens = makeTetragonalTensor(5.6e10, 5.145e10, 2.2e10, 10.6e10, 2.65e10, 6.6e10);
   double rho = 5.96e3;
@@ -25,9 +115,10 @@ int main() {
       cout << "sphere{<" << rad.at(0) << ", " << rad.at(2) << ", " << rad.at(1) << "> 0.01 pigment { color<0, 0, 1> }}" << endl;
     }
   }
-  
+
   return 0;
 }
+*/
 
 //Дима: Сцена 3D - сечения плоскостями (001), (010), (110), с поляризациями
 //Паша: полностью отрисованные поверхности медленности шариками, сечение плоскостью. Плоскость поворачивается
--- crystal/util.cpp
+++ crystal/util.cpp
@@ -7,6 +7,7 @@
 
 #include <cmath>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -40,35 +41,35 @@ Tensor4 makeTetragonalTensor(double c11, double c12, double c13, double c33, dou
         if(b==4) k=0, l=2;
         if(b==5) k=0, l=1;
 
-	double v = tens2[b+6*a];
+    double v = tens2[b+6*a];
         new_tens.set(i,j,k,l,v);
-	new_tens.set(j,i,k,l,v);
-	new_tens.set(i,j,l,k,v);
-	new_tens.set(j,i,l,k,v);
-	new_tens.set(k,l,i,j,v);
-	new_tens.set(l,k,i,j,v);
-	new_tens.set(l,k,j,i,v);
-	new_tens.set(k,l,j,i,v);
+    new_tens.set(j,i,k,l,v);
+    new_tens.set(i,j,l,k,v);
+    new_tens.set(j,i,l,k,v);
+    new_tens.set(k,l,i,j,v);
+    new_tens.set(l,k,i,j,v);
+    new_tens.set(l,k,j,i,v);
+    new_tens.set(k,l,j,i,v);
     }
     return new_tens;
 }
 
 Matrix christoffel(const Tensor4& tens4, const Vec3& n) {
   Matrix new_matrix(3,3);
-  
+
     //\Gamma_il= c_ijkl * n_j * n_k;
     for(int i=0; i<3; ++i) {
         for(int l=0; l<3; ++l) {
-	  double val = 0;
-	  for(int j=0; j<3; ++j) {
-	    for(int k=0; k<3; ++k) {
-	      val += tens4.at(i,j,k,l) * n.at(j) * n.at(k);
-	    }
-	  }
-	  new_matrix.Set(i, l, val);
+      double val = 0;
+      for(int j=0; j<3; ++j) {
+        for(int k=0; k<3; ++k) {
+          val += tens4.at(i,j,k,l) * n.at(j) * n.at(k);
+        }
+      }
+      new_matrix.Set(i, l, val);
         }
     }
-  
+
     return new_matrix;
 }
 
@@ -77,12 +78,12 @@ Polynom MatrixToPoly(const Matrix& mat)
 {
   Matrix mat2 = mat*mat;
   Polynom Pol(1,2,3,4);
-  double a1,a2,a3,a4; // polynomial coefficents at x^3, x^2, x^1, x^0; 
+  double a1,a2,a3,a4; // polynomial coefficents at x^3, x^2, x^1, x^0;
   a1 = 1;
   a2 = -1*mat.trace();
   a3 = -0.5*mat2.trace() + 0.5*mat.trace()*mat.trace();
   a4 = -1*mat.det();
-  Pol.set(a1,a2,a3,a4); 
+  Pol.set(a1,a2,a3,a4);
   return Pol;
 }
 
@@ -129,7 +130,7 @@ Matrix eval(const Matrix& a, double gamma){
   Matrix b(3,3);
   b = a;
   for(int i = 0; i < 3; ++i){
-    b[i][i] = a[i][i] - gamma; 
+    b[i][i] = a[i][i] - gamma;
   }
   return b;
 }
@@ -137,7 +138,7 @@ Matrix eval(const Matrix& a, double gamma){
 vector<SolPart> solveChristoffel(const Matrix& chrMat, double rho){
   vector<SolPart> ret(3);
   vector<double> roots;
-  Matrix Eigen(3,3);  
+  Matrix Eigen(3,3);
   Polynom Pol;
   Vec3 polvector;
   Pol = MatrixToPoly(chrMat);
@@ -153,7 +154,7 @@ vector<SolPart> solveChristoffel(const Matrix& chrMat, double rho){
 Matrix rotX(double a)
 {
   Matrix dat(3,3);
-  
+
   dat.Set(0, 0, 1);
   dat.Set(0, 1, 0);
   dat.Set(0, 2, 0);
@@ -172,7 +173,7 @@ return dat;
 Matrix rotY(double a)
 {
   Matrix dat(3,3);
-  
+
   dat.Set(0, 0, cos(a));
   dat.Set(0, 1, 0);
   dat.Set(0, 2, -sin(a));
@@ -192,7 +193,7 @@ return dat;
 Matrix rotZ(double a)
 {
   Matrix dat(3,3);
-  
+
   dat.Set(0, 0, cos(a));
   dat.Set(0, 1, sin(a));
   dat.Set(0, 2, 0);
@@ -206,4 +207,107 @@ Matrix rotZ(double a)
   dat.Set(2, 2, 1);
 
 return dat;
+}
+
+
+// POVray stuff
+#include <fstream>
+
+#include <math.h>
+#include <string.h>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+#include "povray_templates.h"
+
+PovrayColor::PovrayColor(string html)
+{
+    if((html.size()!=7)||(html.at(0)!='#'))
+    {
+        rgb[2] = rgb[1] = rgb[0] = 255;
+        return;
+    }
+
+    int index=0;
+    for(size_t i = 1;i<html.size();i+=2)
+    {
+        index = i/2;
+        rgb[index] = 0;
+        if((html.at(i)>='0')&&(html.at(i)<='9'))
+        {
+            rgb[index] = (html.at(i)-'0')<<4;
+        }
+        else if((html.at(i)>='A')&&(html.at(i)<='F'))
+        {
+            rgb[index] = (0xA+(html.at(i)-'A'))<<4;
+        }
+        else if((html.at(i)>='a')&&(html.at(i)<='f'))
+        {
+            rgb[index] = (0xA+(html.at(i)-'a'))<<4;
+        }
+        else
+        {
+            //invalid syntax
+            rgb[2] = rgb[1] = rgb[0] = 255;
+            return;
+        }
+
+        if((html.at(i+1)>='0')&&(html.at(i+1)<='9'))
+        {
+            rgb[index] |= (html.at(i+1)-'0');
+        }
+        else if((html.at(i+1)>='A')&&(html.at(i+1)<='F'))
+        {
+            rgb[index] |= 0xA+(html.at(i+1)-'A');
+        }
+        else if((html.at(i+1)>='a')&&(html.at(i+1)<='f'))
+        {
+            rgb[index] |= 0xA+(html.at(i+1)-'a');
+        }
+        else
+        {
+            //invalid syntax
+            rgb[2] = rgb[1] = rgb[0] = 255;
+            return;
+        }
+    }
+}
+
+ostream& operator <<(ostream& os, const PovrayColor& color)
+{
+    return os << "<"<<color.red()<<","<<color.green()<<","<<color.blue()<<">";
+}
+
+ostream& outputPovrayCoords(ostream& os, double x, double y, double z)
+{
+    return os<<"<"<<y<<","<<z<<","<<-1*x<<">";
+}
+
+ostream& outputPovrayCoords(ostream& os, const Vec3& vec)
+{
+    return os<<"<"<<vec.at(1)<<","<<vec.at(2)<<","<<(-1*vec.at(0))<<">";
+}
+
+ostream& outputPovraySphere(ostream& os, const Vec3& vec, const PovrayColor& color)
+{
+    os<<"sphere {\n    ";
+    outputPovrayCoords(os,vec);
+    os<<", 0.04\npigment { rgb"<<color<<" }\n"
+             "    finish {\n        ambient .2\n        diffuse .6\n        specular .75\n"
+             "        roughness .001\n    }\n}\n\n";
+    return os;
+}
+
+std::ostream& outputPovrayCylinder(std::ostream& os, const Vec3& base,
+                                   const Vec3& cap, double radius,
+                                   const PovrayColor& color)
+{
+    os<<"cylinder {\n";
+    outputPovrayCoords(os,base)<<"\n";
+    outputPovrayCoords(os,cap)<<"\n"
+                              <<radius<<"\n"
+                                "pigment{color "<<color<<"}\n}\n";
+    return os;
 }
