b393c00b8b968a7639fb5cdd8ed7823bb66797e8
Andrey <akorolko@rambler.ru>
2013-10-29 07:16:27 +0100

    bugs

--- crystal/Tensor4.cpp
+++ crystal/Tensor4.cpp
@@ -22,15 +22,15 @@ ostream& operator <<(ostream& os, const Tensor4& t) {
 	return os;
 }
 void Tensor4::tensorRot(const Matrix& m){
-  for(int i=0;i < 2 ; ++i){
-    for(int j=0;j < 2 ; ++j){
-      for(int k=0;k < 2 ; ++k){
-	for(int l=0;l < 2 ; ++l){
+  for(int i=0;i < 3 ; ++i){
+    for(int j=0;j < 3 ; ++j){
+      for(int k=0;k < 3 ; ++k){
+	for(int l=0;l < 3 ; ++l){
 	  double svertka;
-	  for(int i1=0;i1 < 2 ; ++i1){
-	    for(int j1=0;j1 < 2 ; ++j1){
-	      for(int k1=0;k1 < 2 ; ++k1){
-		for(int l1=0;l1 < 2 ; ++l1){
+	  for(int i1=0;i1 < 3 ; ++i1){
+	    for(int j1=0;j1 < 3 ; ++j1){
+	      for(int k1=0;k1 < 3 ; ++k1){
+		for(int l1=0;l1 < 3 ; ++l1){
 		  svertka += at(i,j,k,l)*m.Get(i1,i)*m.Get(j1,j)*m.Get(k1,k)*m.Get(l1,l);
 		}  
 	      }
--- crystal/Tensor4.h
+++ crystal/Tensor4.h
@@ -17,6 +17,7 @@ public:
 
        	Tensor4() : v(81) {}
 	void tensorRot(const Matrix& m);
+
 	void set(int i, int j, int k, int l, double x) {
 		checkRE(i, j, k, l);
 		v[27*i + 9*j + 3*k + l] = x;
